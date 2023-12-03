461dd1df9f4814f9c3ca6517307f25cb04528b55
Mai Gia Thon <thonmaigia@gmail.com>
2023-12-03 14:20:13 +0700

    Initial commit

---
+++ src/test/java/WebCafe/WebCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package WebCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
