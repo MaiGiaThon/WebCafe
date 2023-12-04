9989d04cf99590f6df90b5763e8f772f416a2320
Mai Gia Thon <thonmaigia@gmail.com>
2023-12-04 10:21:55 +0700

    All API of Category

---
+++ src/main/java/WebCafe/Dao/CategoryDao.java
@@ -0,0 +1,12 @@
+package WebCafe.Dao;
+
+import WebCafe.Model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryDao extends JpaRepository<Category, Integer> {
+
+    List<Category> getAllCategory();
+
+}
