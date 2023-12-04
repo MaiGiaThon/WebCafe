03a646bd4c921cfdf674b61da3ec41ff66d5c6b2
Mai Gia Thon <thonmaigia@gmail.com>
2023-12-04 10:22:12 +0700

    All API of Category

---
+++ src/main/java/WebCafe/RestImpl/CategoryRestImpl.java
@@ -0,0 +1,52 @@
+package WebCafe.RestImpl;
+
+import WebCafe.Constents.CafeConstants;
+import WebCafe.Model.Category;
+import WebCafe.Rest.CategoryRest;
+import WebCafe.Service.CategoryService;
+import WebCafe.Ultils.CafeUltils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class CategoryRestImpl implements CategoryRest {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Override
+    public ResponseEntity<String> addNewCategory(Map<String, String> requestMap) {
+        try{
+            return categoryService.addNewCategory(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUltils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<Category>> getAllCategory(String filterValue) {
+        try{
+            return categoryService.getAllCategory(filterValue);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateCategory(Map<String, String> requestMap) {
+        try{
+            return categoryService.updateCategory(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUltils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
